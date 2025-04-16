6665d2c04a3f447b91336a29d736d733bfa1e7c9
mrinmoy-765 <mrinmoyd629@gmail.com>
2025-04-16 14:34:38 +0600

    job filters

--- app/Http/Controllers/jobsController.php
+++ app/Http/Controllers/jobsController.php
@@ -10,17 +10,64 @@ use App\Models\jobs_post;
 class jobsController extends Controller
 {
     //this method will show jobs page
-    public function index(){
+    public function index(Request $request)
+    {
 
 
         $categories = Category::where('status', 1)->get();
         $jobTypes = jobType::where('status', 1)->get();
 
-        $jobs = jobs_post::where('status',1)->with('jobType')->orderBy('created_at','DESC')->paginate(9);
-         return view('front.jobs',[
+        $jobs = jobs_post::where('status', 1);
+
+        //search using keyword
+        if (!empty($request->keyword)) {
+            $jobs = $jobs->where(function ($query) use ($request) {
+                $query->orWhere('title', 'like', '%' . $request->keyword . '%');
+                $query->orWhere('keywords', 'like', '%' . $request->keyword . '%');
+            });
+        }
+
+        //search using location
+        if (!empty($request->location)) {
+            $jobs = $jobs->where('location', $request->location);
+        }
+
+        //search using category
+        if (!empty($request->category)) {
+            $jobs = $jobs->where('category_id', $request->category);
+        }
+
+        //search using job type
+        $jobTypeArray = [];
+        if (!empty($request->job_type)) {
+            $jobTypeArray = $request->job_type;
+            $jobs = $jobs->whereIn('job_type_id', $jobTypeArray);
+        }
+
+
+        //search using experience
+        if (!empty($request->experience)) {
+            $jobs = $jobs->where('experience', $request->experience);
+        }
+
+
+
+        $jobs = $jobs->with('jobType');
+
+        if($request->sort == 0){
+            $jobs = $jobs->orderBy('created_at', 'ASC');
+        } else{
+            $jobs = $jobs->orderBy('created_at', 'DESC');
+        }
+       
+
+        $jobs = $jobs->paginate(9);
+
+        return view('front.jobs', [
             'categories' => $categories,
             'jobTypes' => $jobTypes,
             'jobs' => $jobs,
-         ]);
+            'jobTypeArray' => $jobTypeArray
+        ]);
     }
 }
